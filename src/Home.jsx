7faf4e99c0fd319cd7b2baaa299eca70fbbdd9ae
miguel-ls1 <rm95373@estudante.fieb.edu.br>
2025-05-16 12:59:45 +0000

    pokemon

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,23 @@
+import poke from './assets/pokeapi_256.png'
+import eevee from './assets/eevee.jpg'
+import './Home.css'
+
 const Home =()=>{
     return (
         <div>
             <p>Routes blood Routes</p>
+            <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
+            <img src={poke}/>
+            <img src={eevee}/>
         </div>
     )
 }
